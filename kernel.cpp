fcfc076ba58efef772f07c668cd7f8646e412d12
rexionmars <opensource.leonardi@gmail.com>
2023-06-21 21:30:49 -0300

    [refactor] Removed call constructors

--- kernel.cpp
+++ kernel.cpp
@@ -1,19 +1,13 @@
-
 void writeln(char* str)
 {
-    static unsigned short* VideoMemory = (unsigned short*)0xb80000;
+    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
 
     for(int i = 0; str[i] != '\0'; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
-typedef void (*constructor)();
-extern "C" constructor start_ctors;
-extern "C" constructor end_ctors;
-
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magic_number)
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*magic_number*/)
 {
-    writeln("Poison OS");
+    writeln("Poison Operation System --- github.com/rexionmars");
     while(1);
 }
-
